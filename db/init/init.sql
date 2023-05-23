98ba48a8e9e5a67963de8e90868fadd5d0482ebc
ywe <youngwook4146@gmail.com>
2023-05-23 13:55:21 +0900

    Fix : Removed unneeded files

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS toonsql
 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 USE toonsql;
 
 CREATE TABLE IF NOT EXISTS user(
@@ -24,15 +25,24 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 original_name VARCHAR(255) NOT NULL,
 file_path VARCHAR(255) NOT NULL,
 file_size INT NOT NULL,
+file_title VARCHAR(255) NOT NULL,
+file_owner INT NOT NULL,
 created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
+# INSERT INTO files (original_name, file_path, file_size) VALUES ('aaa', 'here', 899);
+# UPDATE files SET original_name = "qqq", file_size = 12312, file_title = "qqq" WHERE id = 1;
 
 select * from user;
 select * from feed;
 select * from files;
 
-DELETE from user WHERE id=1;
 
-drop table user;
-drop table feed;
+
+# DELETE from user WHERE id=1;
+# DELETE from files WHERE id = 3;
+
+# drop table user;
+# drop table feed;
 drop table files;
